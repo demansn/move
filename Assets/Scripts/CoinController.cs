6e0ec107d2e03d681be1557c838363b9709c0487
demansn <demansn@gmail.com>
2014-03-13 16:22:42 +0200

    Added coin (CoinController, model, material and texture). Deleted assets.

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinController : MonoBehaviour {
+
+	public float rotateSpeed = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up * rotateSpeed * Time.deltaTime, Space.World);
+	}
+}
